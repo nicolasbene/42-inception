6ca7c8742757853caf85729a02d6c7d5806098d9
nicolasbene <nicolas.benoit.pro@gmail.com>
2023-10-11 23:51:33 +0200

    commit branch main

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-sed -ie "s/\$WORDPRESS_USER/$WORDPRESS_USER/g" /var/www/html/wordpress/wp-config.php
-sed -ie "s/\$WORDPRESS_PASSWD/$WORDPRESS_PASSWD/g" /var/www/html/wordpress/wp-config.php
-sed -ie "s/\$WORDPRESS_DATABASE/$WORDPRESS_DATABASE/g" /var/www/html/wordpress/wp-config.php
+sed -ie "s/\$MYSQL_USER/$MYSQL_USER/g" /var/www/html/wordpress/wp-config.php
+sed -ie "s/\$MYSQL_PASSWD/$MYSQL_PASSWD/g" /var/www/html/wordpress/wp-config.php
+sed -ie "s/\$MYSQL_DATABASE/$MYSQL_DATABASE/g" /var/www/html/wordpress/wp-config.php
 
 wp core install --path='/var/www/html/wordpress' --url='localhost' \
   --title="Nicolas's Blog" --admin_user='nicolas' --admin_password='nicolas' \
