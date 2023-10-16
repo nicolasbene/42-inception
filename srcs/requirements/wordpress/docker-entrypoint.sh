eda2b90911c341c9339191276ad313229d7e67bf
nicolas <nicolas.benoit.pro@gmail.com>
2023-10-16 12:38:42 +0200

    Add env variable for Wordpress User Author

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -4,8 +4,13 @@ sed -ie "s/\$MYSQL_USER/$MYSQL_USER/g" /var/www/html/wordpress/wp-config.php
 sed -ie "s/\$MYSQL_PASSWD/$MYSQL_PASSWD/g" /var/www/html/wordpress/wp-config.php
 sed -ie "s/\$MYSQL_DATABASE/$MYSQL_DATABASE/g" /var/www/html/wordpress/wp-config.php
 
-wp core install --path='/var/www/html/wordpress' --url='localhost' \
-  --title="Nicolas's Blog" --admin_user='nicolas' --admin_password='nicolas' \
-  --admin_email='nibenoit@student.42.fr' --allow-root
+wp core install \
+    --path='/var/www/html/wordpress' \
+    --url='localhost' \
+    --title="Nicolas's Blog" \
+    --admin_user='nicolas' \
+    --admin_password='nicolas' \
+    --admin_email='nibenoit@student.42.fr' \
+    --allow-root
 
 exec $@
